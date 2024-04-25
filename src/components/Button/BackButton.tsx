d0b95e45e61a946ba0f49cfceb78695af11bbf38
Mathchety <matheusmarchetti1502@gmail.com>
2024-04-25 16:43:58 -0300

    Update headers

--- src/components/Button/BackButton.tsx
+++ src/components/Button/BackButton.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import { View, TouchableOpacity, StyleSheet } from 'react-native';
 import { AntDesign } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+import Home from '../../screens/home/Home';
 
 const BackButton = () => {
+    const navigation = useNavigation();
+    
+    const handleGoHome= () => {
+        navigation.navigate('Home');
+        }
+    
     return (
         <View style={styles.container}>
             <TouchableOpacity
-                onPress={() => { }}            >
+                onPress={handleGoHome}            >
                 <AntDesign name="left" size={24} color="black" />
             </TouchableOpacity>
         </View>
